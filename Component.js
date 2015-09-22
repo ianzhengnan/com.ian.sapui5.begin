e67e84a099f30c1eeaef187262829f6924d5b2a1
Ian Zheng <ian.zheng@sap.com>
2015-09-22 18:39:04 +0800

    'update'

--- Component.js
+++ Component.js
@@ -1,14 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/resource/ResourceModel"
-], function(UIComponent, JSONModel, ResourceModel){
+	"sap/ui/model/json/JSONModel"
+], function(UIComponent, JSONModel){
 	"use strict";
 
 	return UIComponent.extend("com.ian.sapui5.begin.Component", {
 
 		metadata: {
-			rootView: "com.ian.sapui5.begin.view.App"
+			manifest: "json"
 		},
 
 		init: function(){
@@ -25,12 +24,6 @@ sap.ui.define([
 			var oModel = new JSONModel(oData);
 			this.setModel(oModel);
 
-			//set i18n model
-			var i18nModel = new ResourceModel({
-				bundleName: "com.ian.sapui5.begin.i18n.i18n"
-			});
-
-			this.setModel(i18nModel, "i18n");
 		}
 	});
 

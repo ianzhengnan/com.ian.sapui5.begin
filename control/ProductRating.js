924274a1cacd1a638a0abbd4d5331eebdb8ff995
Ian Zheng <ian.zheng@sap.com>
2015-10-13 20:55:53 +0800

    'update'

---
+++ control/ProductRating.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	"sap/ui/core/Controller"
+], function(Controller){
+	"use strict";
+
+	return Controller.extend("com.ian.sapui5.begin.control.ProductRating", {
+		metadata: {
+
+		},
+		init: function(){
+
+		},
+		renderer: function(oRM, oControl){
+
+		}
+	});
+
+});
